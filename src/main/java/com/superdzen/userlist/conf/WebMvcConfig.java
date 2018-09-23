3b12ab2ea52256faaaee249d16e87129479d8ce3
superdzen <superdzen@gmail.com>
2018-09-23 13:45:59 +0700

    XML config replaced by Java-based config

--- src/main/java/com/superdzen/userlist/conf/WebMvcConfig.java
+++ src/main/java/com/superdzen/userlist/conf/WebMvcConfig.java
@@ -1,48 +1,22 @@
 package com.superdzen.userlist.conf;
 
-import com.superdzen.userlist.dao.UserDaoImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebMvc
 @PropertySource("classpath:application.properties")
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Autowired
-    private Environment env;
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("//**").addResourceLocations("//");
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-
-        return dataSource;
-    }
-
-    @Bean
-    public UserDaoImpl userDaoImpl() {
-
-        return new UserDaoImpl();
     }
 
     @Bean
