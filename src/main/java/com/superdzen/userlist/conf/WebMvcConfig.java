1190eb2554110571a2bb7407f147a9ee27e5a358
superdzen <superdzen@gmail.com>
2018-09-23 13:59:35 +0700

    Removing excessive config

--- src/main/java/com/superdzen/userlist/conf/WebMvcConfig.java
+++ src/main/java/com/superdzen/userlist/conf/WebMvcConfig.java
@@ -1,18 +1,30 @@
 package com.superdzen.userlist.conf;
 
+import com.superdzen.userlist.dao.UserDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebMvc
+@ComponentScan("com.superdzen.userlist")
 @PropertySource("classpath:application.properties")
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -28,4 +40,30 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         return viewResolver;
     }
 
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+
+    @Bean(name = "transactionManager")
+    public DataSourceTransactionManager getTransactionManager() {
+        DataSourceTransactionManager txManager = new DataSourceTransactionManager();
+
+        DataSource dataSource = this.getDataSource();
+        txManager.setDataSource(dataSource);
+
+        return txManager;
+    }
+
+    @Bean
+    public UserDaoImpl userDaoImpl() {
+
+        return new UserDaoImpl();
+    }
 }
